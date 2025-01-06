5d2b846b573e70f633aef453861ca191d9072c25
Piotr <p.pawlowski88@gmail.com>
2025-01-07 00:28:32 +0100

    dodano glowny plik

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ import pycaret
 load_dotenv()
 init_notebook_mode(all_interactive=True)
 model_runner = joblib.load('marathon_pipeline_regression_model.pkl')
-openai_client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+#openai_client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 instructor_openai_client = instructor.from_openai(openai_client)
 llm_client = LangfuseOpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
@@ -117,6 +117,22 @@ def convert_seconds_to_hhmmss(total_seconds):
 
 
 ###MAIN###
+
+# Kod zabezpieczający klucz API
+if not st.session_state.get("openai_api_key"):
+    if "OPENAI_API_KEY" in load_dotenv:
+        st.session_state["openai_api_key"] = load_dotenv["OPENAI_API_KEY"]
+    else:
+        st.info("Podaj klucz API aby korzystać z Creative Paintings")
+        st.session_state["openai_api_key"] = st.text_input("Klucz API", type="password")
+        if st.session_state["openai_api_key"]:
+            st.rerun()
+if not st.session_state.get("openai_api_key"):
+    st.stop()
+
+openai_client = OpenAI(api_key=st.session_state["openai_api_key"])
+
+
 
 def main():
     st.title("Marathon Road Runner")
